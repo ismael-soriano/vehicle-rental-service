1b436d50b0ba37fb980805c77a05c425649c0a22
Ismael Soriano <ismaelsorianolozano@gmail.com>
2026-08-18 23:59:46 +0200

    feat(infrastructure): support rental update on return

--- src/VehicleRental.Microservice.Infrastructure/Persistence/RentalRepository.cs
+++ src/VehicleRental.Microservice.Infrastructure/Persistence/RentalRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using VehicleRental.Microservice.Domain.Rentals;
+using VehicleRental.Microservice.Domain.Vehicles;
 
 namespace VehicleRental.Microservice.Infrastructure.Persistence
 {
@@ -10,5 +11,8 @@ namespace VehicleRental.Microservice.Infrastructure.Persistence
 
         public async Task<bool> HasActiveRentalForCustomer(CustomerId customerId) =>
             await context.Rentals.AnyAsync(r => r.CustomerId == customerId && r.ReturnedAt == null);
+
+        public async Task<Rental> GetActiveByVehicle(VehicleId vehicleId) =>
+            await context.Rentals.SingleOrDefaultAsync(r => r.VehicleId == vehicleId && r.ReturnedAt == null);
     }
 }
